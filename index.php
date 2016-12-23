fe5646caba8d1261f17dd62cedec87e141bdc00a
Alex Sinkevich <alexandr290497@gmail.com>
2016-12-23 03:11:55 +0200

    q

--- index.php
+++ index.php
@@ -2,49 +2,49 @@
 <html lang="en,ru">
 <head>
 	<meta charset="UTF-8">
-	<title>MyShopOnline</title>
-	<meta name="descripton" content="Мой интернет магазин"/>
-	<meta name="keywords" content="Одежда,товары, покупки онлайн">
+	<title>StartPage</title>
+	<meta name = "description" content = "Гитара и всё что с ней связано"/>
+	<meta name = "keywords" content = "Табы, аккорды, ноты, гитара, песни"/>
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+	
 	<div class="main">
 		<div class="header">
 			<div class="logo">
-				<div class="logo_text">
-					<h1><a href="index.php">Мой Магазин</a></h1>
-					<h4>Добро пожаловать</h4>
-				</div>	
+				<h1><a href="index.php">GuitarHub</a></h1>
 			</div>
 			<div class="menubar">
 				<ul class="menu">
 					<li class="selected"><a href="index.php">Главная</a></li>
-					<li> <a href="#">Мужская одежда</a></li>
-					<li> <a href="#">Женская одежда</a></li>
-					<li> <a href="#">Детская одежда</a></li>
-					<li> <a href="#">Контакты</a></li>
-				</ul>
+					<li><a href="#">Новости</a></li>
+					<li><a href="#">Материалы</a></li>
+					<li><a href="#">Магазин</a></li>
+					<li><a href="#">О нас</a></li>
+				</ul>	
 			</div>
 		</div>
-		<form action="login.php" method="post">
-			<p>
-				<label>Login:<br></label>
-				<input name="login" type="text" size="15" maxlength="15">
-			</p>
+	</div>
+<div class="auth">
+	<form action="login.php" method="post">
+		<p>
+			<label>Login:<br></label>
+			<input name="login" type="text" size="15" maxlength="15">
+		</p>
 
-			<p>
-				<label>Password:<br></label>
-				<input name="password" type="password" size="15" maxlength="15">
-			</p>
+		<p>
+			<label>Password:<br></label>
+			<input name="password" type="password" size="15" maxlength="15">
+		</p>
 
-			<p>
-				<input type="submit" name="submit" value="Войти">
+		<p>
+			<input type="submit" name="submit" value="Войти">
+			<br>
 
-				<!--**** Кнопочка (type="submit") отправляет данные на страничку testreg.php ***** --> 
-				<br>
-				<!--**** ссылка на регистрацию, ведь как-то же должны гости туда попадать ***** --> 
-				 
-			</p></form>	
-			<a href="reg.php">Registration</a>
-		</body>
-		</html>
+		</p></form>	
+		<a href="reg.php">Registration</a></div>
+</div>
+	
+
+	</body>
+	</html>
