2c76ae1f9772b831d06722671e78f326999ad0e5
Alex Sinkevich <alexandr290497@gmail.com>
2017-01-05 12:17:38 +0200

    new

--- index.php
+++ index.php
@@ -8,7 +8,6 @@
 	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-	
 	<div class="main">
 		<div class="header">
 			<div class="logo">
@@ -16,35 +15,56 @@
 			</div>
 			<div class="menubar">
 				<ul class="menu">
-					<li class="selected"><a href="index.php">Главная</a></li>
-					<li><a href="#">Новости</a></li>
-					<li><a href="#">Материалы</a></li>
-					<li><a href="#">Магазин</a></li>
-					<li><a href="#">О нас</a></li>
+					<li class="selected">
+						<a href="index.php">Главная</a>
+					</li>
+					<li>
+						<a href="#">Новости </a>
+						<ul class="sub">
+							<li><a href="#">Продукты</a></li>
+							<li><a href="#">Команда</a></li>
+						</ul>
+
+					</li>
+					<li><a href="#">Материалы </a>
+					<ul class="sub">
+							<li><a href="#">Продукты</a></li>
+							<li><a href="#">Команда</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Магазин </a></li>
+					<li><a href="#">Информация </a></li>
 				</ul>	
+			</div>
+		
+		</div>
+		<button class="show_popup_login" rel="popup1">Вход</button>
+			
+		</div>
+
+		<div class="overlay_popup">
+			<div class="auth" id="popup1">
+				<form action="login.php" method="post">
+					<p>
+						<label>Login:<br></label>
+						<input name="login" type="text" size="15" maxlength="15">
+					</p>
+					<p>
+						<label>Password:<br></label>
+						<input name="password" type="password" size="15" maxlength="15">
+					</p>
+
+					<p>
+						<input type="submit" name="submit" value="Войти">
+						<br>
+					</p>
+				</form>	
+				<a href="reg.php">Registration</a>
 			</div>
 		</div>
 	</div>
-<div class="auth">
-	<form action="login.php" method="post">
-		<p>
-			<label>Login:<br></label>
-			<input name="login" type="text" size="15" maxlength="15">
-		</p>
-
-		<p>
-			<label>Password:<br></label>
-			<input name="password" type="password" size="15" maxlength="15">
-		</p>
-
-		<p>
-			<input type="submit" name="submit" value="Войти">
-			<br>
-
-		</p></form>	
-		<a href="reg.php">Registration</a></div>
-</div>
 	
-
+	<script src="scripts.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 	</body>
 	</html>
--- reg.php
+++ reg.php
@@ -1,50 +1,50 @@
 <html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-    <title>Регистрация</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<title>Регистрация</title>
 </head>
 <body>
 
-<form action="reg.php" method="POST" />
-    <table>
-        <tr>
-            <td>Введите логин:</td>
-            <td><input type="text" size="20" name="login" /></td>
-        </tr>
-        <tr>
-            <td>Введите пароль:</td>
-            <td><input type="password" size="20" maxlength="20" name="password" /></td>
-        </tr>
-        <tr>
-            <td colspan="2"><input class="button" type="submit" value="Зарегистрироваться" name="submit" /></td>
-        </tr>
-    </table>
+	<form action="reg.php" method="POST" />
+	<table>
+		<tr>
+			<td>Введите логин:</td>
+			<td><input type="text" size="20" name="login" /></td>
+		</tr>
+		<tr>
+			<td>Введите пароль:</td>
+			<td><input type="password" size="20" maxlength="20" name="password" /></td>
+		</tr>
+		<tr>
+			<td colspan="2"><input class="button" type="submit" value="Зарегистрироваться" name="submit" /></td>
+		</tr>
+	</table>
 </form>
 
 <?php
-$connection = mysqli_connect('localhost', 's97248pw_beta', 'JOker290497', 's97248pw_beta') or die(mysqli_error($connection));
+$connection = mysqli_connect('localhost', 's97248pw_beta', '*63K%ACX', 's97248pw_beta') or die(mysqli_error($connection));
 
 if (isset($_POST['submit'])) 
 {
-    if (empty($_POST['login'])) 
-    {
-        $info_reg = 'Вы не ввели Логин';
-    }
-    elseif (empty($_POST['password'])) 
-    {
-        $info_reg = 'Вы не ввели пароль';
-    }                      
-    else 
-    {
-        $login = $_POST['login'];             
-        $password = $_POST['password'];
-  
-        $query = "INSERT INTO `users` (login, password)
-        VALUES ('$login', '$password')";
-        $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-                    
-        $info_reg = 'Вы успешно зарегистрировались!';
-    }
+	if (empty($_POST['login'])) 
+	{
+		$info_reg = 'Вы не ввели Логин';
+	}
+	elseif (empty($_POST['password'])) 
+	{
+		$info_reg = 'Вы не ввели пароль';
+	}                      
+	else 
+	{
+		$login = $_POST['login'];             
+		$password = md5($_POST['password']);
+		
+		$query = "INSERT INTO `users` (login, password)
+		VALUES ('$login', '$password')";
+		$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+		
+		$info_reg = 'Вы успешно зарегистрировались!';
+	}
 }
 
 $info_reg = isset($info_reg) ? $info_reg : NULL;
